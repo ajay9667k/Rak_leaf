22293b16c9927b220564b8918c0e73d7a933bb8b
Ajay Kumar <ajay9667k@gmail.com>
2025-07-02 11:05:33 +0530

    Initial commit

--- src/componets/features.jsx
+++ src/componets/features.jsx
@@ -64,7 +64,7 @@ const Features = () => {
 
   return (
     <section className="py-20 bg-[#07282C]  text-white">
-      <div className="max-w-7xl mx-auto md:mt-0 mt-[100px] px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto md:mt-0 mt-[160px] px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-5xl font-extrabold mb-4 text-[#71D6CE] ">Powerful Network Features</h2>
           <p className="text-lg text-gray-200">
